8ff71c65fbef54bf753ebb8fd9917dc3263bac86
Baktiar Miah <baktiar.m@gmail.com>
2019-11-17 18:13:36 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,8 +7,8 @@ class CreateHauntedHouses  < ActiveRecord::Migration[5.2]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.timestamp :opening_time
-      t.timestamp :closing_time
+      t.timestamp :opening_date
+      t.timestamp :closing_date
       t.text :long_description
     end
   end
