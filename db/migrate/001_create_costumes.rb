347167bd6114192d1a8f158c099e7504199c00d5
Baktiar Miah <baktiar.m@gmail.com>
2019-11-17 18:05:29 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,7 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
       t.string :name
       t.integer :price
       t.string :size
-      t.sring :image_url
+      t.string :image_url
       t.timestamps
     end
  end
